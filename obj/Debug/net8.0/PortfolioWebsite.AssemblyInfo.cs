4222b95a0e5aa711470599731f9bcaa86f81ec1c
SamarthLoni <samarthloni1111.com>
2026-06-26 12:36:05 +0530

    removed doc format resume

--- obj/Debug/net8.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc912f08b822991d739a9f9e01762f67264c8268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44e1577799279da959d1b2d723e4e16e88d023d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
